a73671f707198e003da94282c8a5fcb522fae575
expl0iit <daniovision@yandex.com>
2018-07-27 13:00:38 +0200

    Added an ENV variable, made db login switch automatically based on it.

--- v1/incl/headers.php
+++ v1/incl/headers.php
@@ -1,4 +1,7 @@
 <?php
+define('ENV', 'development');
+//define('ENV', 'production');
+
 if(isset($_SERVER['HTTP_SESSION_ID'])) session_id($_SERVER['HTTP_SESSION_ID']);
 if(isset($_POST['session_id'])) session_id($_POST['session_id']);
 session_start();
